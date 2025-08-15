2c7cf4cd48f933ea2a6e6d80512c79e9732fc7cf
Sandeep Panwar <sandeeppanwar7310@gmail.com>
2025-08-15 10:56:58 +0530

    gateway

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ console.log(button);
 const login="Login page added";
 console.log(login);
 
+const Payment ="Integrated the payment gateway";
+console.log(Payment);
 const upi="upi is added";
 console.log(upi);
