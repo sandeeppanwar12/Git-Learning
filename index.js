378e4823054ac14fa7ab4783545636b805f70cb3
Sandeep Panwar <sandeeppanwar7310@gmail.com>
2025-08-16 18:08:42 +0530

    final

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const button ="Added a Button";
-console.log(button);
-
-const login="Login page added";
-console.log(login);
-
-const Payment ="Integrated the payment gateway";
-console.log(Payment);
-const upi="upi is added";
-console.log(upi);
-
-
-//fixing the branch
-console.log(Bug_fixed);
+// Optional: Add smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
